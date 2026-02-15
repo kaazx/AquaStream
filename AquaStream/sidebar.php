15bfca7e426e7481b86e60913697e83f7cab8fce
Kirsten Zapanta <keithleenzapanta@gmail.com>
2026-02-15 14:21:33 +0800

    Add sidebar navigation to AquaStream

---
+++ AquaStream/sidebar.php
@@ -0,0 +1,34 @@
+<aside class="sidebar" id="sidebar">
+        <div class="sidebar-header">
+            <img src="imgs/logo.png" alt="AquaStream Logo" class="logo">
+        </div>
+        
+        <nav class="sidebar-nav">
+            <ul class="nav-list">
+                <li class="nav-item">
+                    <a href="Dashboard.php" class="nav-link">
+                        <img src="imgs/dashboard-icon.png" alt="Dashboard Icon" class="nav-icon">
+                        <span class="nav-text">Dashboard</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="CreateOrder.php" class="nav-link active">
+                        <img src="imgs/createorder-icon.png" alt="Create Order Icon" class="nav-icon">
+                        <span class="nav-text">Create Order</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="OrdersList.php" class="nav-link">
+                        <img src="imgs/orderslist-icon.png" alt="Orders List Icon" class="nav-icon">
+                        <span class="nav-text">Orders List</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="History.php" class="nav-link">
+                        <img src="imgs/history-icon.png" alt="History Icon" class="nav-icon">
+                        <span class="nav-text">History</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+</aside>
