bbf7fef4c119b65cb8d2af7733ec47f315fb7df4
Kirsten Zapanta <keithleenzapanta@gmail.com>
2026-02-16 11:06:30 +0800

    Add footer section to AquaStream website

---
+++ AquaStream/footer.php
@@ -0,0 +1,16 @@
+<footer>
+    <div class="footer-content">
+        <div class="footer-logo">
+            <img src="imgs/logo.png" alt="AquaStream">
+        </div>
+        <p class="tagline">Delivers A Steady, Continuous Flow of Water</p>
+        <div class="social-links">
+            <img src="imgs/fb_icon.png" alt="Facebook">
+            <img src="imgs/ig_icon.png" alt="Instagram">
+            <img src="imgs/whatsapp_icon.png" alt="WhatsApp">
+        </div>
+    </div>
+    <div class="ctto">
+        <p>All Rights Reserved</p>
+    </div>
+</footer>
